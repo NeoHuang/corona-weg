adc70feccb28681064e554b5b66c71095e6225f3
neohuang <superneohuang@gmail.com>
2020-03-06 22:41:37 +0100

    Add api name

--- api/api.go
+++ api/api.go
@@ -11,6 +11,7 @@ import (
 )
 
 type Api interface {
+	Name() string
 	GetCurrent() (core.EpidemicMap, error)
 }
 
@@ -18,6 +19,7 @@ type ExtractFunc func(*goquery.Document) core.EpidemicMap
 
 type GeneralApi struct {
 	url       string
+	name      string
 	extractFn ExtractFunc
 
 	cachePeriod      time.Duration
@@ -26,12 +28,17 @@ type GeneralApi struct {
 	mutex            sync.Mutex
 }
 
-func NewGeneralApi(endpointUrl string, extractFn ExtractFunc, cachePeriod time.Duration) *GeneralApi {
+func NewGeneralApi(endpointUrl string, name string, extractFn ExtractFunc, cachePeriod time.Duration) *GeneralApi {
 	return &GeneralApi{
 		url:         endpointUrl,
+		name:        name,
 		extractFn:   extractFn,
 		cachePeriod: cachePeriod,
 	}
+}
+
+func (api *GeneralApi) Name() string {
+	return api.name
 }
 
 func (api *GeneralApi) GetCurrent() (core.EpidemicMap, error) {
@@ -46,17 +53,17 @@ func (api *GeneralApi) GetCurrent() (core.EpidemicMap, error) {
 
 	res, err := http.Get(api.url)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get http request:%s", err)
+		return nil, fmt.Errorf("Api %q failed to get http request:%s", api.name, err)
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+		return nil, fmt.Errorf("Api %q status code error: %d %s", api.name, res.StatusCode, res.Status)
 	}
 
 	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse body:%s", err)
+		return nil, fmt.Errorf("Api %q failed to parse body:%s", api.name, err)
 	}
 
 	epidemicMap := api.extractFn(doc)
@@ -67,9 +74,9 @@ func (api *GeneralApi) GetCurrent() (core.EpidemicMap, error) {
 }
 
 func NewRkiApi(cachePeriod time.Duration) Api {
-	return NewGeneralApi("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html", RkiExtractFunc, cachePeriod)
+	return NewGeneralApi("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html", "RKI", RkiExtractFunc, cachePeriod)
 }
 
 func NewJetztApi(cachePeriod time.Duration) Api {
-	return NewGeneralApi("https://www.coronavirus.jetzt/karten/deutschland/", JetztExtractFunc, cachePeriod)
+	return NewGeneralApi("https://www.coronavirus.jetzt/karten/deutschland/", "Jetzt", JetztExtractFunc, cachePeriod)
 }
